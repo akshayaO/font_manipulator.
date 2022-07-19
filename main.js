cd69862bdfe2195101c428b69bd4417da3250424
akshayaO <79805752+akshayaO@users.noreply.github.com>
2022-07-18 19:24:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(350,350);
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+
+}
+
+function modelLoaded(){
+    console.log('Model got loaded!');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log('results');
+    }
+}
